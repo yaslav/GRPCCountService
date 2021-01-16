ca9d818149f7d8908c2ace056b9603132db11ff1
Slava <slava.yavchenko@gmail.com>
2021-01-16 22:48:55 +0100

    Implement grpc server listening on port 4567

--- GRPCCountService.cpp
+++ GRPCCountService.cpp
@@ -1,17 +1,30 @@
 ﻿// GRPCCountService.cpp: Definiert den Einstiegspunkt für die Anwendung.
 //
 
+#include <string>
 #include <grpcpp/grpcpp.h>
 #include "counter.grpc.pb.h"
-#include "counter.pb.h"
 
 #include "GRPCCountService.h"
+#include "CountServiceImplementation.hpp"
 
 
+void RunServer(std::string address) {
+	CountServiceImplementation service;
+	grpc::ServerBuilder builder;
+	builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+	
+	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+	std::cout << "Listening on port: " << address << std::endl;
+
+	server->Wait();
+}
 int main()
 {
-	counter::CountRequest req;
-	req.set_count(10);
-	std::cout << req.count() << std::endl;
+
+	RunServer("0.0.0.0:4567");
 	return 0;
 }
+
+
