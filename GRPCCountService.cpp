880e75c31dacb2c0c6b7febacbf8230d5aba8f4a
Slava <slava.yavchenko@gmail.com>
2021-01-16 22:01:55 +0100

    Initial commit: starter files to create grpc service in c++ and python

---
+++ GRPCCountService.cpp
@@ -0,0 +1,17 @@
+﻿// GRPCCountService.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include <grpcpp/grpcpp.h>
+#include "counter.grpc.pb.h"
+#include "counter.pb.h"
+
+#include "GRPCCountService.h"
+
+
+int main()
+{
+	counter::CountRequest req;
+	req.set_count(10);
+	std::cout << req.count() << std::endl;
+	return 0;
+}
